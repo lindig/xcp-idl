1b243578da05bf02b0ecff39363d751e806d5ab6
Christian Lindig <christian.lindig@citrix.com>
2017-01-11 17:21:55 +0000

    Remove whitespace at the end of lines.
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- rrd_ppx/rrd_idl.ml
+++ rrd_ppx/rrd_idl.ml
@@ -14,29 +14,29 @@ module My = struct
     units : string
   } [@@deriving rpcty]
 
-  type plugin_protocol = V1 | V2 [@@deriving rpcty] 
-  type sampling_frequency = Five_Seconds [@@deriving rpcty] 
+  type plugin_protocol = V1 | V2 [@@deriving rpcty]
+  type sampling_frequency = Five_Seconds [@@deriving rpcty]
 
   module Statefile_latency = struct
-    type t = {id: string; latency: float option} [@@deriving rpcty] 
+    type t = {id: string; latency: float option} [@@deriving rpcty]
   end
 
 
   type interdomain_uid = {
     name: string;
     frontend_domid: int;
-  } [@@deriving rpcty] 
+  } [@@deriving rpcty]
 
   type interdomain_info = {
     frequency: sampling_frequency;
     shared_page_refs: int list;
-  } [@@deriving rpcty] 
+  } [@@deriving rpcty]
 
-  
-  type error = 
-    | Archive_failed_exn of string 
+
+  type error =
+    | Archive_failed_exn of string
   [@@deriving rpcty]
-  
+
   exception Error of error
 
   let raiser = function
@@ -62,7 +62,7 @@ let (//) = Filename.concat
 let default_sockets_dir = "/var/lib/xcp"
 let daemon_name     = "xcp-rrdd"
 let default_path    = ref (default_sockets_dir // daemon_name)
-let forwarded_path  = 
+let forwarded_path  =
   ref (default_sockets_dir // daemon_name ^ ".forwarded")
 
 let set_sockets_dir x =
@@ -77,14 +77,14 @@ module API(R: Idl.RPC) = struct
   module T = struct (* RPC call parameter type representation *)
 
     (* base types *)
-    let int     = Idl.Param.mk Rpc.Types.int 
-    let int32   = Idl.Param.mk Rpc.Types.int32 
-    let int64   = Idl.Param.mk Rpc.Types.int64 
-    let bool    = Idl.Param.mk Rpc.Types.bool 
-    let float   = Idl.Param.mk Rpc.Types.float 
-    let string  = Idl.Param.mk Rpc.Types.string 
-    let char    = Idl.Param.mk Rpc.Types.char 
-    let unit    = Idl.Param.mk Rpc.Types.unit 
+    let int     = Idl.Param.mk Rpc.Types.int
+    let int32   = Idl.Param.mk Rpc.Types.int32
+    let int64   = Idl.Param.mk Rpc.Types.int64
+    let bool    = Idl.Param.mk Rpc.Types.bool
+    let float   = Idl.Param.mk Rpc.Types.float
+    let string  = Idl.Param.mk Rpc.Types.string
+    let char    = Idl.Param.mk Rpc.Types.char
+    let unit    = Idl.Param.mk Rpc.Types.unit
 
     let option ~name ~description ty = Idl.Param.mk
         ~name
@@ -130,10 +130,10 @@ module API(R: Idl.RPC) = struct
         ~description:["remote address"]
         Rpc.Types.string
 
-    let address_opt = option 
+    let address_opt = option
         ~name:"address"
         ~description:["remote address"]
-        Rpc.Types.(Basic String) 
+        Rpc.Types.(Basic String)
 
     let path = Idl.Param.mk
         ~name:"path"
@@ -183,10 +183,10 @@ module API(R: Idl.RPC) = struct
 
   (* declare RPC function using the types defined above *)
 
-  let has_vm_rrd = R.declare 
+  let has_vm_rrd = R.declare
       "has_vm_rrd"
       ["True if we have RRD data for the given VM"]
-      R.(T.vm_uuid @-> returning T.bool My.error) 
+      R.(T.vm_uuid @-> returning T.bool My.error)
 
   let push_rrd_local = R.declare
       "push_rrd_local"
@@ -194,144 +194,144 @@ module API(R: Idl.RPC) = struct
       R.(T.vm_uuid @-> T.domid @-> returning T.unit My.error)
 
   let remove_rrd = R.declare
-      "remove_rrd" 
-      ["remove_rrd"] 
+      "remove_rrd"
+      ["remove_rrd"]
       R.(T.rrd_uuid @-> returning T.unit My.error)
 
   let migrate_rrd = R.declare
-      "migrate_rrd" 
-      ["migrate_rrd"] 
-      R.(T.session_id 
-         @-> T.address 
-         @-> T.vm_uuid 
+      "migrate_rrd"
+      ["migrate_rrd"]
+      R.(T.session_id
+         @-> T.address
+         @-> T.vm_uuid
          @-> T.host_uuid
          @-> returning T.unit My.error
         )
 
   let send_host_rrd_to_master = R.declare
-      "send_host_rrd_to_master" 
-      ["send_host_rrd_to_master"] 
+      "send_host_rrd_to_master"
+      ["send_host_rrd_to_master"]
       R.(T.address @-> returning T.unit My.error)
 
   let backup_rrds = R.declare
-      "backup_rrds" 
-      ["backup_rrds"] 
+      "backup_rrds"
+      ["backup_rrds"]
       R.(T.address_opt @-> T.unit @-> returning T.unit My.error)
 
   let archive_rrd = R.declare
-      "archive_rrd" 
-      ["archive_rrd"] 
+      "archive_rrd"
+      ["archive_rrd"]
       R.(T.vm_uuid @-> T.address_opt @-> returning T.unit My.error)
 
   let archive_sr_rrd = R.declare
-      "archive_sr_rrd" 
-      ["archive_sr_rrd"] 
+      "archive_sr_rrd"
+      ["archive_sr_rrd"]
       R.(T.sr_uuid @-> returning T.string My.error)
 
   let push_sr_rrd = R.declare
-      "push_sr_rrd" 
-      ["push_sr_rrd"] 
+      "push_sr_rrd"
+      ["push_sr_rrd"]
       R.(T.sr_uuid @-> T.path @-> returning T.unit My.error)
 
   let add_host_ds = R.declare
-      "add_host_ds" 
-      ["add_host_ds"] 
+      "add_host_ds"
+      ["add_host_ds"]
       R.(T.ds_name @-> returning T.unit My.error)
 
   let forget_host_ds = R.declare
-      "forget_host_ds" 
-      ["forget_host_ds"] 
+      "forget_host_ds"
+      ["forget_host_ds"]
       R.(T.ds_name @-> returning T.unit My.error)
 
   let query_possible_host_dss = R.declare
-      "query_possible_host_dss" 
-      ["query_possible_host_dss"] 
+      "query_possible_host_dss"
+      ["query_possible_host_dss"]
       R.(T.unit @-> returning T.datasources My.error)
 
   let query_host_ds = R.declare
-      "query_host_ds" 
-      ["query_host_ds"] 
+      "query_host_ds"
+      ["query_host_ds"]
       R.(T.ds_name @-> returning T.float My.error)
 
   let add_vm_ds = R.declare
-      "add_vm_ds" 
-      ["add_vm_ds"] 
-      R.(T.vm_uuid 
-         @-> T.domid 
-         @-> T.ds_name 
+      "add_vm_ds"
+      ["add_vm_ds"]
+      R.(T.vm_uuid
+         @-> T.domid
+         @-> T.ds_name
          @-> returning T.unit My.error
         )
 
   let forget_vm_ds = R.declare
-      "forget_vm_ds" 
-      ["forget_vm_ds"] 
+      "forget_vm_ds"
+      ["forget_vm_ds"]
       R.(T.vm_uuid @-> T.ds_name @-> returning T.unit My.error)
 
   let query_possible_vm_dss = R.declare
-      "query_possible_vm_dss" 
-      ["query_possible_vm_dss"] 
+      "query_possible_vm_dss"
+      ["query_possible_vm_dss"]
       R.(T.vm_uuid @-> returning T.datasources My.error)
 
   let query_vm_ds = R.declare
-      "query_vm_ds" 
-      ["query_vm_ds"] 
+      "query_vm_ds"
+      ["query_vm_ds"]
       R.(T.vm_uuid @-> T.ds_name @-> returning T.float My.error)
 
   let add_sr_ds = R.declare
-      "add_sr_ds" 
-      ["add_sr_ds"] 
+      "add_sr_ds"
+      ["add_sr_ds"]
       R.(T.sr_uuid @-> T.ds_name @-> returning T.unit My.error)
 
   let forget_sr_ds = R.declare
-      "forget_sr_ds" 
-      ["forget_sr_ds"] 
+      "forget_sr_ds"
+      ["forget_sr_ds"]
       R.(T.sr_uuid @-> T.ds_name @-> returning T.unit My.error)
 
   let query_possible_sr_dss = R.declare
-      "query_possible_sr_dss" 
-      ["query_possible_sr_dss"] 
+      "query_possible_sr_dss"
+      ["query_possible_sr_dss"]
       R.(T.sr_uuid @-> returning T.datasources My.error)
 
   let query_sr_ds = R.declare
-      "query_sr_ds" 
-      ["query_sr_ds"] 
+      "query_sr_ds"
+      ["query_sr_ds"]
       R.(T.sr_uuid @-> T.ds_name @-> returning T.float My.error)
 
   let update_use_min_max = R.declare
-      "update_use_min_max" 
-      ["update_use_min_max"] 
+      "update_use_min_max"
+      ["update_use_min_max"]
       R.(T.bool @-> returning T.unit My.error)
 
   let update_vm_memory_target = R.declare
-      "update_vm_memory_target" 
-      ["update_vm_memory_target"] 
+      "update_vm_memory_target"
+      ["update_vm_memory_target"]
       R.(T.domid @-> T.memory @-> returning T.unit My.error)
 
   let set_cache_sr = R.declare
-      "set_cache_sr" 
-      ["set_cache_sr"] 
+      "set_cache_sr"
+      ["set_cache_sr"]
       R.(T.sr_uuid @-> returning T.unit My.error)
 
   let unset_cache_sr = R.declare
-      "unset_cache_sr" 
-      ["unset_cache_sr"] 
+      "unset_cache_sr"
+      ["unset_cache_sr"]
       R.(T.unit @-> returning T.unit My.error)
 
   module Plugin = struct
     let get_header = R.declare
-        "get_header" 
-        ["get_header"] 
+        "get_header"
+        ["get_header"]
         R.(T.unit @-> returning T.string My.error)
 
     let get_path = R.declare
-        "get_path" 
-        ["get_path"] 
+        "get_path"
+        ["get_path"]
         R.(T.plugin_id @-> returning T.string My.error)
 
     module Local = struct
       let register = R.declare
-          "register" 
-          ["register"] 
+          "register"
+          ["register"]
           R.(T.plugin_id
              @-> T.sampling_frequency
              @-> T.protocol
@@ -339,60 +339,60 @@ module API(R: Idl.RPC) = struct
             )
 
       let deregister = R.declare
-          "deregister" 
-          ["deregister"] 
+          "deregister"
+          ["deregister"]
           R.(T.plugin_id @-> returning T.unit My.error)
 
       let next_reading = R.declare
-          "next_reading" 
-          ["next_reading"] 
+          "next_reading"
+          ["next_reading"]
           R.(T.plugin_id @-> returning T.float My.error)
 
     end
 
     module Interdomain = struct
       let register = R.declare
-          "register" 
-          ["register"] 
+          "register"
+          ["register"]
           R.(T.interdomain_id
-             @-> T.interdomain_info 
+             @-> T.interdomain_info
              @-> T.protocol
              @-> returning T.float My.error
             )
 
       let deregister = R.declare
-          "deregister" 
-          ["deregister"] 
+          "deregister"
+          ["deregister"]
           R.(T.interdomain_id @-> returning T.unit My.error)
 
       let next_reading = R.declare
-          "next_reading" 
-          ["next_reading"] 
+          "next_reading"
+          ["next_reading"]
           R.(T.interdomain_id @-> returning T.float My.error)
     end
 
     let register = R.declare
-        "register" 
-        ["register"] 
-        R.(T.plugin_id  
-           @-> T.sampling_frequency 
+        "register"
+        ["register"]
+        R.(T.plugin_id
+           @-> T.sampling_frequency
            @-> returning T.float My.error
           )
 
     let deregister = R.declare
-        "deregister" 
-        ["deregister"] 
+        "deregister"
+        ["deregister"]
         R.(T.plugin_id @-> returning T.unit My.error)
 
     let next_reading = R.declare
-        "next_reading" 
-        ["next_reading"] 
+        "next_reading"
+        ["next_reading"]
         R.(T.plugin_id @-> returning T.float My.error)
   end
   module HA = struct
     let enable_and_update = R.declare
-        "enable_and_update" 
-        ["enable_and_update"] 
+        "enable_and_update"
+        ["enable_and_update"]
         R.(T.list
              ~name:"statefile_latencies"
              ~description:["statefile latencies"]
@@ -409,24 +409,24 @@ module API(R: Idl.RPC) = struct
           )
 
     let disable = R.declare
-        "disable" 
-        ["disable"] 
+        "disable"
+        ["disable"]
         R.(T.unit @-> returning T.unit My.error)
   end
 
   module Deprecated = struct
     let load_rrd = R.declare
-        "load_rrd" 
-        ["load_rrd"] 
-        R.(T.plugin_id  
+        "load_rrd"
+        ["load_rrd"]
+        R.(T.plugin_id
            @-> Idl.Param.mk
              ~name:"timescale"
              ~description:["timescale"]
              Rpc.Types.int
-           @-> T.option 
+           @-> T.option
              ~name:"address"
              ~description:["master address"]
-             Rpc.Types.(Basic String) 
+             Rpc.Types.(Basic String)
            @-> returning T.unit My.error
           )
   end
